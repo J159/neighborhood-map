e7c42fe6a6b90399958c7a353a7bd45e73fda68e
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-18 03:40:50 -0500

    Create basic html structure, add title

--- src/App.js
+++ src/App.js
@@ -6,20 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="title">
+          <h1>Edgewater, NJ</h1>
+          <p>Places I would like to visit after (hopefully) passing Nanodegree</p>
         </header>
+        <nav id="sidebar">Testing</nav>
+        // Map component will go here>
       </div>
     );
   }
