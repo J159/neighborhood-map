e7e117dc613823b3122e32e3472bf841bd0cd50c
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-19 02:13:35 -0500

    feat: Display Google Map, add proper credentials

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import {GoogleApiWrapper, Map} from 'google-maps-react';
 
 class GoogleMap extends Component {
   render() {
+    const style = {
+        width: '100%',
+        height: '100vh',
+        float: 'right',
+        overflow: 'hidden'
+    }
     return (
-      <div>
-        Test
-      </div>
+      <Map
+        google={this.props.google}
+        style={style}
+        zoom={14}
+        initialCenter={
+          {lat: 40.815989, lng: -73.97997}
+        }
+      >
+      </Map>
     );
   }
 }
 
-export default GoogleMap;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCyWz-7NRnDzdE0kkFMlZ3HGNa7avBIOec"
+})(GoogleMap)
