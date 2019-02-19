bee76b55fa5c74a95fda9220209d91aec13bd7dc
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-18 23:35:48 -0500

    Create GoogleMap.js file

---
+++ src/GoogleMap.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class GoogleMap extends Component {
+  render() {
+    return (
+      <div>
+        Test
+      </div>
+    );
+  }
+}
+
+export default GoogleMap;
