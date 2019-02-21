40e9fec68dab2effc73bc95e781dd52ee0f1f804
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-21 02:40:09 -0500

    Add temporary drop animation to each Marker

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -34,24 +34,6 @@ class GoogleMap extends Component {
       float: 'right'
     }
 
-    let locationArray = [
-      {name: 'Mitsuwa Marketplace', position: {lat: 40.8159891, lng: -73.97997}},
-      {name: 'Kinokuniya', position: {lat: 40.816703942419494, lng: -73.97981464862823}},
-      {name: 'Target', position: {lat: 40.814061, lng: -73.981719}},
-      {name: 'SoJo Spa Club', position: {lat: 40.818949, lng: -73.979436}},
-      {name: 'Sushi Cruise', position: {lat: 40.818186, lng: -73.97709}},
-      {name: 'Edgewater Multiplex Cinemas', position: {lat: 40.807979, lng: -73.98806}},
-      {name: 'McDonalds', position: {lat: 40.813605, lng: -73.9838}},
-      {name: 'T.J.Maxx', position: {lat: 40.811844, lng: -73.984927}},
-      {name: 'Kuppi Coffee Company', position: {lat: 40.818919, lng: -73.9768}},
-      {name: 'Pet Valu', position: {lat: 40.820082, lng: -73.977218}},
-      // might remove below
-      {name: 'Genji Sushi, Gyoza, & Ramen Bar', position: {lat: 40.825034, lng: -73.97393}},
-      {name: 'Karaoke Bar', position: {lat: 40.825339, lng: -73.974858}}
-    ];
-
-
-
     return (
       <Map
         onClick={this.onMapClicked}
@@ -71,54 +53,65 @@ class GoogleMap extends Component {
           onClick={this.onMarkerClick}
           name='Mitsuwa Marketplace'
           position={{lat: 40.8159891, lng: -73.97997}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <Marker
           onClick={this.onMarkerClick}
           name='Kinokuniya'
           position={{lat: 40.816703942419494, lng: -73.97981464862823}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <Marker
           onClick={this.onMarkerClick}
           name='Target'
           position={{lat: 40.814061, lng: -73.981719}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <Marker
           onClick={this.onMarkerClick}
           name='SoJo Spa Club'
           position={{lat: 40.818949, lng: -73.979436}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <Marker
           onClick={this.onMarkerClick}
           name='Sushi Cruise'
           position={{lat: 40.818186, lng: -73.97709}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <Marker
           onClick={this.onMarkerClick}
           name='Edgewater Multiplex Cinemas'
           position={{lat: 40.807979, lng: -73.98806}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <Marker
           onClick={this.onMarkerClick}
           name='McDonalds'
           position={{lat: 40.813605, lng: -73.9838}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <Marker
           onClick={this.onMarkerClick}
           name='T.J.Maxx'
           position={{lat: 40.811844, lng: -73.984927}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <Marker
           onClick={this.onMarkerClick}
           name='Kuppi Coffee Company'
           position={{lat: 40.818919, lng: -73.9768}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <Marker
           onClick={this.onMarkerClick}
           name='Pet Valu'
           position={{lat: 40.820082, lng: -73.977218}}
+          animation={this.props.google.maps.Animation.DROP}
         />
         <InfoWindow
           marker={this.state.activeMarker}
+          animation={this.props.google.maps.Animation.BOUNCE}
           visible={this.state.showInfoWindow}>
             <div>
               <h1>{this.state.selectedPlace.name}</h1>
