5b77f654376985adc425a7f08fe4adab0228c24c
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-21 03:08:38 -0500

    Map through locations to display Sidebar list

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -9,19 +9,31 @@ class Sidebar extends Component {
   updateQuery = (query) => {
     // set this.updateSearchResults within this.setState to guarantte order
     this.setState({ query }, () => {
-      this.updateSearchResults(query)
+      // this.updateSearchResults(query)
+      console.log(query);
+      this.locationArray.forEach(location => {
+        console.log(location)
+      })
     })
   }
 
   render() {
     return (
-      <nav id="sidebar">Testing
+      <nav id="sidebar">
         <input
           type="text"
           placeholder="Search"
           value={this.state.query}
           onChange={(event) => this.updateQuery(event.target.value)}
         />
+      <ul>
+        {this.props.locations.map((location, key) =>
+          <li key={location.id}>
+            {location.name}
+          </li>
+        )}
+      </ul>
+
       </nav>
     );
   }
