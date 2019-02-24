28a2f99d8efb79b77f96401b97afaf04996dc5ae
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-24 17:03:58 -0500

    refactor: Adjust code to retrieve updated props from App.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,36 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Sidebar extends Component {
-  state = {
-    query: "",
-    searchResults: this.props.locations
-  }
-
-  updateQuery = (query) => {
-    this.setState({ query }, () => {
-      this.updateSidebar(query)
-    })
-  }
-
-  updateSidebar = (query) => {
-    let updateSidebarResultes = this.props.locations.filter(location =>
-      location.name.toLowerCase().includes(query.toLowerCase())
-    );
-    this.setState({ searchResults: updateSidebarResultes})
-    console.log(this.state.searchResults)
-  }
-
   render() {
     return (
       <nav id="sidebar">
         <input
           type="text"
           placeholder="Search"
-          value={this.state.query}
-          onChange={(event) => this.updateQuery(event.target.value)}
+          value={this.query}
+          onChange={(event) => this.props.updateQuery(event.target.value)}
         />
         <ul id="results">
-          {this.state.searchResults.map((location, key) =>
+          {this.props.locations.map((location, key) =>
             <li key={key}>
               <button>{location.name}</button>
             </li>
