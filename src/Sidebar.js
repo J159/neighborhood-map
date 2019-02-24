ba7f9ac824d965b0b745cb529ec7419e3ce3e5de
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-24 06:05:48 -0500

    Add searchResults array and updateSidebar function

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,18 +3,23 @@ import './App.css';
 
 class Sidebar extends Component {
   state = {
-    query: ""
+    query: "",
+    searchResults: []
   }
 
   updateQuery = (query) => {
-    // set this.updateSearchResults within this.setState to guarantte order
     this.setState({ query }, () => {
-      // this.updateSearchResults(query)
-      console.log(query);
-      this.locationArray.forEach(location => {
-        console.log(location)
-      })
+      this.updateSidebar(query)
     })
+  }
+
+  updateSidebar = (query) => {
+    let updateSidebarResultes = this.props.locations.filter(location =>
+      location.name.toLowerCase().includes(query.toLowerCase()));
+    this.setState({ searchResults: updateSidebarResultes})
+    let sidebar = document.getElementById("results")
+    sidebar.innerHTML = <li><button>{this.state.searchResults}</button></li>
+    console.log(this.state.searchResults)
   }
 
   render() {
@@ -26,10 +31,10 @@ class Sidebar extends Component {
           value={this.state.query}
           onChange={(event) => this.updateQuery(event.target.value)}
         />
-      <ul>
+      <ul id="results">
         {this.props.locations.map((location, key) =>
-          <li key={location.id}>
-            {location.name}
+          <li key={location.name}>
+            <button>{location.name}</button>
           </li>
         )}
       </ul>
